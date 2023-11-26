ae5f9edf9a99d78239b3261325e90d1ca5c7e306
mrdth <mrdth@zeventien.org>
2023-11-26 18:57:47 +0000

    "Change start URL in PWA config to /notes
    The start URL in the PWA manifest configuration was previously '/'. This has been changed to '/notes'. The change was made to ensure that the application points to the correct path when opened as a PWA. The inclusion of ', ' at the end of each array element ensures consistency with Laravel's coding style."

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -5,44 +5,44 @@ return [
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
         'short_name' => 'PWA',
-        'start_url' => '/',
+        'start_url' => '/notes',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
         'display' => 'standalone',
-        'orientation'=> 'any',
-        'status_bar'=> 'black',
+        'orientation' => 'any',
+        'status_bar' => 'black',
         'icons' => [
             '72x72' => [
                 'path' => '/images/icons/icon-72x72.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '96x96' => [
                 'path' => '/images/icons/icon-96x96.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '128x128' => [
                 'path' => '/images/icons/icon-128x128.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '144x144' => [
                 'path' => '/images/icons/icon-144x144.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '152x152' => [
                 'path' => '/images/icons/icon-152x152.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '192x192' => [
                 'path' => '/images/icons/icon-192x192.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '384x384' => [
                 'path' => '/images/icons/icon-384x384.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '512x512' => [
                 'path' => '/images/icons/icon-512x512.png',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
         ],
         'splash' => [
@@ -64,15 +64,15 @@ return [
                 'url' => '/shortcutlink1',
                 'icons' => [
                     "src" => "/images/icons/icon-72x72.png",
-                    "purpose" => "any"
-                ]
+                    "purpose" => "any",
+                ],
             ],
             [
                 'name' => 'Shortcut Link 2',
                 'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
-            ]
+                'url' => '/shortcutlink2',
+            ],
         ],
-        'custom' => []
-    ]
+        'custom' => [],
+    ],
 ];
